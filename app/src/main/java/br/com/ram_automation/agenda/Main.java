8d8c5c948cbab590b1bb66b226c78e8f7a4b886b
Rafael_VAIO <r.marcelino@ram-automation.com.br>
2020-01-07 16:13:39 -0300

    Updating Activity

--- app/src/main/java/br/com/ram_automation/agenda/Main.java
+++ app/src/main/java/br/com/ram_automation/agenda/Main.java
@@ -1,7 +1,19 @@
 package br.com.ram_automation.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 public class Main extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView nome = new TextView(this);
+        nome.setText("Rafael Alves Marcelino");
 
+        setContentView(nome);
+    }
 }
