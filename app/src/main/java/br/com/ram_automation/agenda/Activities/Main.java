b9e424a4567dd7a99aa8800b7c463704625392b7
Rafael Alves Marcelino <ram.programacao@gmail.com>
2020-01-07 16:46:11 -0300

    Added static layout

--- app/src/main/java/br/com/ram_automation/agenda/Activities/Main.java
+++ app/src/main/java/br/com/ram_automation/agenda/Activities/Main.java
@@ -1,19 +1,19 @@
-package br.com.ram_automation.agenda;
+package br.com.ram_automation.agenda.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import br.com.ram_automation.agenda.R;
 
 public class Main extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView nome = new TextView(this);
-        nome.setText("Rafael Alves Marcelino");
-
-        setContentView(nome);
+        setContentView(R.layout.activity_main);
     }
 }
