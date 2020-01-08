6f6ace28f780c5bc677b4ba851b1c84dd2b1e49d
Rafael_DELL <ram.programacao@gmail.com>
2020-01-07 22:53:51 -0300

    Inserting ListAdapter and Relative Layout

--- app/src/main/java/br/com/ram_automation/agenda/Activities/Main.java
+++ app/src/main/java/br/com/ram_automation/agenda/Activities/Main.java
@@ -1,12 +1,20 @@
 package br.com.ram_automation.agenda.Activities;
 
 import android.app.Activity;
+import android.opengl.EGLExt;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 import br.com.ram_automation.agenda.R;
 
@@ -15,5 +23,18 @@ public class Main extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>();
+        alunos.add("Rafael");
+        alunos.add("Carmem");
+        alunos.add("Barbara");
+        alunos.add("Vera");
+        alunos.add("Domingos");
+        alunos.add("Aline");
+        alunos.add("Rochele");
+
+        ListView lv_alunos = findViewById(R.id.activity_main_lv_alunos);
+
+        lv_alunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,alunos));
     }
 }
